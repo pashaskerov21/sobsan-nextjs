ed230d7a6d7bae3eae08af81f571be7fc881dbd7
pasha <askerovpasha21@gmail.com>
2024-01-08 09:58:18 +0400

    menu toggle problem solved

--- src/partials/header/Header.tsx
+++ src/partials/header/Header.tsx
@@ -22,62 +22,45 @@ const Header: React.FC<HeaderProps> = ({
   toggleTheme,
 }) => {
   const pathName = usePathname();
-  const [headerState, setHeaderState] = React.useState<HeaderStateType>({
-    fixed: false,
-    menuShow: false,
-    searchShow: false,
-  });
-  React.useEffect(() => {
-    setHeaderState({
-      fixed: false,
-      menuShow: false,
-      searchShow: false,
-    });
-    const body = document.querySelector('body')
-    if (body) {
-      body.style.overflow = headerState.menuShow ? "auto" : "hidden";
-    }
-  }, [pathName])
-  // header fixed
+  const body = document.querySelector('body')
+  const [headerState, setHeaderState] = React.useState<HeaderStateType>({ fixed: false, menuShow: false, searchShow: false, });
   React.useEffect(() => {
     window.addEventListener('scroll', function () {
       if (this.scrollY > 300) {
-        setHeaderState((prev) => ({
-          ...prev,
-          fixed: true,
-        }));
+        setHeaderState((prev) => ({ ...prev, fixed: true, }));
       } else {
-        setHeaderState((prev) => ({
-          ...prev,
-          fixed: false,
-        }));
+        setHeaderState((prev) => ({ ...prev, fixed: false, }));
       }
     });
-
     return () => {
       window.removeEventListener('scroll', () => { });
     }
   }, []);
-  // menu toggle
-
-  const toggleMenu = React.useCallback(() => {
+  React.useEffect(() => {
+    setHeaderState({ fixed: false, menuShow: false, searchShow: false, });
+    if (body) {
+      body.style.overflow = "visible";
+    }
+  },[pathName])
+  const openMenu = React.useCallback(() => {
     if (window.innerWidth < 1200) {
-      setHeaderState((prev) => ({
-        ...prev,
-        menuShow: !headerState.menuShow,
-      }));
-      const body = document.querySelector('body')
+      setHeaderState((prev) => ({ ...prev, menuShow: true }));
       if (body) {
-        body.style.overflow = headerState.menuShow ? "auto" : "hidden";
+        body.style.overflow = "hidden";
+      }
+    }
+  }, [headerState.menuShow]);
+  const closeMenu = React.useCallback(() => {
+    if (window.innerWidth < 1200) {
+      setHeaderState((prev) => ({ ...prev, menuShow: false }));
+      if (body) {
+        body.style.overflow = "visible";
       }
     }
   }, [headerState.menuShow]);
 
   const toggleSearch = React.useCallback(() => {
-    setHeaderState((prev) => ({
-      ...prev,
-      searchShow: !headerState.searchShow,
-    }))
+    setHeaderState((prev) => ({ ...prev, searchShow: !headerState.searchShow }))
   }, [headerState.searchShow])
 
   return (
@@ -94,7 +77,7 @@ const Header: React.FC<HeaderProps> = ({
           titleDictionary={titleDictionary}
           theme={theme}
           toggleTheme={toggleTheme}
-          toggleMenu={toggleMenu}
+          toggleMenu={openMenu}
           toggleSearch={toggleSearch}
         />
         <BottomNavbar
@@ -110,7 +93,7 @@ const Header: React.FC<HeaderProps> = ({
           titleDictionary={titleDictionary}
           theme={theme}
           toggleTheme={toggleTheme}
-          toggleMenu={toggleMenu}
+          toggleMenu={closeMenu}
           toggleSearch={toggleSearch}
         />
       </HeaderWrapper>
