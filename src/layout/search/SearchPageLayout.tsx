ac66ac103be6b9ef846bc8c486c42ea6f8778df4
pasha <askerovpasha21@gmail.com>
2024-02-14 13:23:34 +0400

    new search structure

--- src/layout/search/SearchPageLayout.tsx
+++ src/layout/search/SearchPageLayout.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment } from 'react'
 import { useDispatch } from 'react-redux';
 import { updateLocaleSlug } from '@/src/redux/actions';
 import { PageTitle, ProductGrid, Skeleton } from '@/src/components';
@@ -20,7 +20,7 @@ import {
 import { i18n } from '@/i18n-config';
 import { Container, Section } from '@/src/styles';
 import { Category, Menu, Product } from '@/src/class';
-import { useRouter } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 
 type LayoutProps = {
     activeLocale: LocaleType,
@@ -92,105 +92,88 @@ const SearchPageLayout: React.FC<LayoutProps> = ({
     }
 
 
-    const router = useRouter();
     const product = new Product(productData, productTranslateData);
     const menu = new Menu(menuData, menuTranslateData);
     const category = new Category(categoryData, categoryTranslateData);
-    const [queryStatus, setQueryStatus] = useState<boolean>(false);
-    const [query, setQuery] = useState<string>('');
-    const [searchProducts, setSearchProducts] = useState<ProductDataType[]>([]);
 
-    useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const queryParam = urlParams.get('query');
-        if (queryParam) {
-            setQueryStatus(true);
-            setQuery(queryParam);
-            localeSlugs = i18n.locales.map((locale) => {
-                return {
-                    locale: locale,
-                    slug: `${path}?query=${queryParam}`,
-                }
-            });
-            dispatch(updateLocaleSlug(localeSlugs));
-
-            const searchProductsByTitle: ProductDataType[] = product.searchByTitle(queryParam, activeLocale);
-            const searchProductsByDesc: ProductDataType[] = product.searchByDesc(queryParam, activeLocale);
-            const mergedProducts = [...searchProductsByTitle, ...searchProductsByDesc];
-
-            const uniqueProducts = mergedProducts.filter((product, index, self) =>
-                index === self.findIndex((p) => (
-                    p.id === product.id
-                ))
-            );
-
-            const activeCategoryData: CategoriesDataType | undefined = category.search(queryParam, activeLocale);
-            const activeMenuData: MenuDataType | undefined = menu.search(queryParam, activeLocale);
-            if (uniqueProducts.length > 0) {
-                setQueryStatus(true);
-                setSearchProducts(uniqueProducts);
-            } else if (activeCategoryData) {
-                setQueryStatus(true);
-                const url = category.getTranslate(activeCategoryData.id, activeLocale, "url");
-                router.push(url);
-            } else if (activeMenuData) {
-                setQueryStatus(true);
-                const url = menu.getTranslate(activeMenuData, activeLocale, "url");
-                router.push(url);
-            } else {
-                setQueryStatus(false);
-                setSearchProducts([]);
+    const urlParams = new URLSearchParams(window.location.search);
+    const queryParam = urlParams.get('query');
+    if (queryParam) {
+        localeSlugs = i18n.locales.map((locale) => {
+            return {
+                locale: locale,
+                slug: `${path}?query=${queryParam}`,
             }
+        });
+        dispatch(updateLocaleSlug(localeSlugs));
+
+        const searchProductsByTitle: ProductDataType[] = product.searchByTitle(queryParam, activeLocale);
+        const searchProductsByDesc: ProductDataType[] = product.searchByDesc(queryParam, activeLocale);
+        const mergedProducts = [...searchProductsByTitle, ...searchProductsByDesc];
+
+        const uniqueProducts = mergedProducts.filter((product, index, self) =>
+            index === self.findIndex((p) => (
+                p.id === product.id
+            ))
+        );
+
+        const activeCategoryData: CategoriesDataType | undefined = category.search(queryParam, activeLocale);
+        const activeMenuData: MenuDataType | undefined = menu.search(queryParam, activeLocale);
+
+        if (uniqueProducts.length > 0) {
+            return (
+                <Fragment>
+                    <PageTitle
+                        loading={loading}
+                        activeLocale={activeLocale}
+                        pageTitleData={pageTitleData}
+                        titleDictionary={titleDictionary}
+                    />
+                    <Section $py={20}>
+                        <Container>
+                            <ProductGrid
+                                loading={loading}
+                                activeLocale={activeLocale}
+                                productData={uniqueProducts}
+                                productTranslateData={productTranslateData}
+                                brandData={brandData}
+                                brandTranslateData={brandTranslateData}
+                                generalDictionary={generalDictionary}
+                                className="full__container"
+                                searchKeyword={queryParam}
+                            />
+                        </Container>
+                    </Section>
+                </Fragment>
+            )
+        } else if (activeCategoryData) {
+            redirect(`${category.getTranslate(activeCategoryData.id, activeLocale, "url")}`)
+        } else if (activeMenuData) {
+            redirect(`${menu.getTranslate(activeMenuData, activeLocale, "url")}`)
         } else {
-            setQueryStatus(false);
+            return (
+                <Fragment>
+                    <PageTitle
+                        loading={loading}
+                        activeLocale={activeLocale}
+                        pageTitleData={pageTitleData}
+                        titleDictionary={titleDictionary}
+                    />
+                    <Section $py={20}>
+                        <Container>
+                            {
+                                loading.standart ? <Skeleton width='100%' height='45px' /> : (
+                                    <h3 className='text-center text-lg-start'>{generalDictionary["search_error_message"]}</h3>
+                                )
+                            }
+                        </Container>
+                    </Section>
+                </Fragment>
+            )
         }
-    }, [dispatch]);
-
-
-    return (
-        <Fragment>
-            <PageTitle
-                loading={loading}
-                activeLocale={activeLocale}
-                pageTitleData={pageTitleData}
-                titleDictionary={titleDictionary}
-            />
-            <Section $py={20}>
-                <Container>
-                    {
-                        queryStatus ? (
-                            <Fragment>
-                                {
-                                    searchProducts.length > 0 && (
-                                        <ProductGrid
-                                            loading={loading}
-                                            activeLocale={activeLocale}
-                                            productData={searchProducts}
-                                            productTranslateData={productTranslateData}
-                                            brandData={brandData}
-                                            brandTranslateData={brandTranslateData}
-                                            generalDictionary={generalDictionary}
-                                            className="full__container"
-                                            searchKeyword={query}
-                                        />
-                                    )
-                                }
-                            </Fragment>
-                        ) : (
-                            <Fragment>
-                                {
-                                    loading.standart ? <Skeleton width='100%' height='45px' /> : (
-                                        <h3 className='text-center text-lg-start'>{generalDictionary["search_error_message"]}</h3>
-                                    )
-                                }
-                            </Fragment>
-                        )
-                    }
-
-                </Container>
-            </Section>
-        </Fragment>
-    )
+    } else {
+        redirect(`/${activeLocale}/404`);
+    }
 }
 
 export default React.memo(SearchPageLayout)
